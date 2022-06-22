74c643ca942484d2ec854a2207933e4941ad33ae
algosketch <sjy_9484@naver.com>
2022-06-22 20:34:16 +0900

    fix yml & add test case

--- src/main/java/org/algosketch/jpashop/Member.java
+++ src/main/java/org/algosketch/jpashop/Member.java
@@ -1,2 +1,18 @@
-package org.algosketch.jpashop.member;public class Member {
+package org.algosketch.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
 }
--- src/test/java/org/algosketch/jpashop/MemberRepositoryTest.java
+++ src/test/java/org/algosketch/jpashop/MemberRepositoryTest.java
@@ -1,4 +1,34 @@
-import static org.junit.Assert.*;
+package org.algosketch.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MemberRepositoryTest {
-  
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
 }
